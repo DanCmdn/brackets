c49b5e6edc968ddb425533d58c09837a63a0512d
Danil Chemodanov <dan.cmdn@gmail.com>
2020-10-22 16:37:54 +0300

    feat: 3 tests left

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let arr = [];
+
+	function checkPairs(symb, symbInArr) {
+		if (symbInArr == null) {
+			return false;
+		} else if (symb === ']' && symbInArr === '[') {
+			return true;
+		} else if (symb === ')' && symbInArr === '(') {
+			return true;
+		} else if (symb === '}' && symbInArr === '{') {
+			return true;
+		}
+	}
+
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === '[' || str[i] === '(' || str[i] === '{') {
+			arr.push(str[i]);
+		} else if (str[i] === ']' || str[i] === ')' || str[i] === '}') {
+			
+			if (arr.length === 0) {
+				return false;
+			} else if (checkPairs(str[i], arr[arr.length-1])) {
+				arr.pop();
+			}
+		}
+	}
+	if (arr.length === 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
