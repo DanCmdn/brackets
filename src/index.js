829b5d7fd95a07b5a7c950db76f32c1dec271607
Danil Chemodanov <dan.cmdn@gmail.com>
2020-10-23 14:31:08 +0300

    feat: 3 tests left

--- src/index.js
+++ src/index.js
@@ -4,26 +4,52 @@ module.exports = function check(str, bracketsConfig) {
 	function checkPairs(symb, symbInArr) {
 		if (symbInArr == null) {
 			return false;
-		} else if (symb === ']' && symbInArr === '[') {
-			return true;
-		} else if (symb === ')' && symbInArr === '(') {
-			return true;
-		} else if (symb === '}' && symbInArr === '{') {
-			return true;
-		}
+		};
+		for (let item of bracketsConfig) {
+			if (symb === item[1] && symbInArr === item[0]) {
+				return true;
+			}
+		};
+
+		// if (symbInArr == null) {
+		// 	return false;
+		// } else if (symb === ']' && symbInArr === '[') {
+		// 	return true;
+		// } else if (symb === ')' && symbInArr === '(') {
+		// 	return true;
+		// } else if (symb === '}' && symbInArr === '{') {
+		// 	return true;
+		// }
 	}
 
 	for (let i = 0; i < str.length; i++) {
-		if (str[i] === '[' || str[i] === '(' || str[i] === '{') {
-			arr.push(str[i]);
-		} else if (str[i] === ']' || str[i] === ')' || str[i] === '}') {
-			
-			if (arr.length === 0) {
-				return false;
-			} else if (checkPairs(str[i], arr[arr.length-1])) {
-				arr.pop();
+		for (let item of bracketsConfig) {
+			if (str[i] === item[0]) {
+				if (str[i] === item[1]) {
+					if (checkPairs(str[i], arr[arr.length - 1])) {
+						arr.pop();
+					}
+				} else {
+					arr.push(str[i]);
+
+				}
+			} else if (str[i] === item[1]) {
+				if (arr.length === 0) {
+					return false;
+				} else if (checkPairs(str[i], arr[arr.length - 1])) {
+					arr.pop();
+				} 
 			}
 		}
+		// if (str[i] === '[' || str[i] === '(' || str[i] === '{') {
+		// 	arr.push(str[i]);
+		// } else if (str[i] === ']' || str[i] === ')' || str[i] === '}') {
+			
+		// 	if (arr.length === 0) {
+		// 		return false;
+		// 	} else if (checkPairs(str[i], arr[arr.length-1])) {
+		// 		arr.pop();
+		// 	}
 	}
 	if (arr.length === 0) {
 		return true;
