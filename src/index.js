9ca4135f6f93f8d6f982b7ad3bc1d14254e5898e
Danil Chemodanov <dan.cmdn@gmail.com>
2020-10-25 11:35:50 +0300

    feat: solve all tests

--- src/index.js
+++ src/index.js
@@ -4,22 +4,12 @@ module.exports = function check(str, bracketsConfig) {
 	function checkPairs(symb, symbInArr) {
 		if (symbInArr == null) {
 			return false;
-		};
+		}
 		for (let item of bracketsConfig) {
 			if (symb === item[1] && symbInArr === item[0]) {
 				return true;
 			}
-		};
-
-		// if (symbInArr == null) {
-		// 	return false;
-		// } else if (symb === ']' && symbInArr === '[') {
-		// 	return true;
-		// } else if (symb === ')' && symbInArr === '(') {
-		// 	return true;
-		// } else if (symb === '}' && symbInArr === '{') {
-		// 	return true;
-		// }
+		}
 	}
 
 	for (let i = 0; i < str.length; i++) {
@@ -28,10 +18,11 @@ module.exports = function check(str, bracketsConfig) {
 				if (str[i] === item[1]) {
 					if (checkPairs(str[i], arr[arr.length - 1])) {
 						arr.pop();
+					} else {
+						arr.push(str[i]);
 					}
 				} else {
 					arr.push(str[i]);
-
 				}
 			} else if (str[i] === item[1]) {
 				if (arr.length === 0) {
@@ -41,16 +32,8 @@ module.exports = function check(str, bracketsConfig) {
 				} 
 			}
 		}
-		// if (str[i] === '[' || str[i] === '(' || str[i] === '{') {
-		// 	arr.push(str[i]);
-		// } else if (str[i] === ']' || str[i] === ')' || str[i] === '}') {
-			
-		// 	if (arr.length === 0) {
-		// 		return false;
-		// 	} else if (checkPairs(str[i], arr[arr.length-1])) {
-		// 		arr.pop();
-		// 	}
 	}
+	
 	if (arr.length === 0) {
 		return true;
 	} else {
